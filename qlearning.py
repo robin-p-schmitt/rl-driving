b2d352b795af1eba72108cd1da6fe2a44bb77fbf
Robin Schmitt <schmitt.p.robin@gmail.com>
2022-06-19 10:58:22 +0200

    add separate target net which is updated occasionally with the policy net weights

--- qlearning.py
+++ qlearning.py
@@ -18,9 +18,11 @@ class QLearning:
     self.lr = 0.0001
     self.gamma = 0.9
 
-    self.network = DeepQNetwork(state_size=self.game.state_size, action_size=self.game.action_size)
+    self.policy_net = DeepQNetwork(state_size=self.game.state_size, action_size=self.game.action_size)
+    self.target_net = DeepQNetwork(state_size=self.game.state_size, action_size=self.game.action_size)
     self.optimizer = tf.optimizers.Adam(self.lr)
     self.batch_size = 16
+    self.target_update = 25
 
     self.experiences = []
     self.max_exps = 200
@@ -31,13 +33,17 @@ class QLearning:
 
     print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
+  def update_target_weights(self):
+    for policy_var, target_var in zip(self.policy_net.trainable_variables, self.target_net.trainable_variables):
+      target_var.assign(policy_var.numpy())
+
   def train(self, dt):
     cur_state = self.game.get_state()
     # choose action
     if np.random.rand() < 0.05:
       cur_action = np.random.randint(0, self.game.action_size, size=None)
     else:
-      cur_action = np.argmax(self.network(np.atleast_2d(cur_state)))
+      cur_action = np.argmax(self.policy_net(np.atleast_2d(cur_state)))
 
     reward = self.game.make_action(cur_action, dt)
     next_state = self.game.get_state()
@@ -62,18 +68,18 @@ class QLearning:
       batch = Experience(*zip(*sample_exps))
       cur_states, cur_actions, next_states, rewards = [np.asarray(batch[i]) for i in range(len(batch))]
 
-      targets = rewards + self.gamma * np.max(self.network(np.atleast_2d(next_states)), axis=1)
+      targets = rewards + self.gamma * np.max(self.policy_net(np.atleast_2d(next_states)), axis=1)
       targets = tf.convert_to_tensor(targets, dtype="float32")
 
       # calculate loss
       with tf.GradientTape() as tape:
         predictions = tf.math.reduce_sum(
-          self.network(np.atleast_2d(cur_states).astype("float32")) * tf.one_hot(cur_actions, self.game.action_size),
+          self.policy_net(np.atleast_2d(cur_states).astype("float32")) * tf.one_hot(cur_actions, self.game.action_size),
           axis=1)
         loss = tf.math.reduce_mean(tf.square(targets - predictions))
 
       # update weights
-      weights = self.network.trainable_weights
+      weights = self.policy_net.trainable_weights
       gradients = tape.gradient(loss, weights)
       self.optimizer.apply_gradients(zip(gradients, weights))
 
@@ -83,8 +89,11 @@ class QLearning:
         print("AVG LOSS OVER PAST 100 STEPS AFTER %d updates: %f" % (self.n_updates, loss.numpy() / 100))
         self.total_loss = 0
 
+      if self.n_updates % self.target_update == 0:
+        self.update_target_weights()
+
   def test(self):
-    next_action = np.argmax(self.network(np.atleast_2d(self.game.get_state())))
+    next_action = np.argmax(self.target_net(np.atleast_2d(self.game.get_state())))
     self.game.make_action(next_action)
     print(next_action)
 
